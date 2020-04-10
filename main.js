bf98285cfa1811dcc8da0a0cbae2764853548b99
Adam Dullage <adam@dullage.com>
2020-04-10 13:16:01 +0100

    Styled Electron Titlebar

--- main.js
+++ main.js
@@ -2,15 +2,18 @@ const { app, BrowserWindow } = require("electron");
 
 function createWindow() {
   let win = new BrowserWindow({
+    frame: false,
     width: 500,
-    height: 800
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true
+    }
   });
 
-  win.setMenu(null);
-  win.loadFile("./dist/index.html");
+  win.loadFile("main.html");
 
   // Open dev tools in separate window.
-  //win.webContents.openDevTools({mode:'undocked'});
+  // win.webContents.openDevTools({mode:'undocked'});
 }
 
 app.on("ready", createWindow);
