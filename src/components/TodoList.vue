0643f64ef2de0399d6f27e280737c192a76f6531
Adam Dullage <adam@dullage.com>
2020-04-14 20:49:01 +0100

    Delay drag with mouse also

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -18,7 +18,6 @@
         invert-swap="true"
         class="todos"
         delay="400"
-        delay-on-touch-only="true"
         touch-start-threshold="3"
         force-fallback="true"
       >
