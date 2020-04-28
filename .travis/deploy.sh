5c397883d3bea2bd9955f93a2f26851227d0be42
Adam Dullage <adam@dullage.com>
2020-04-28 21:32:41 +0100

    Debugging ENV

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,5 +6,5 @@ SSH_COMMAND="rm -R $DEPLOY_DIR_DEV/dist && mv $DEPLOY_DIR_DEV/staging/dist $DEPL
 
 openssl aes-256-cbc -K $encrypted_7df0bf92a043_key -iv $encrypted_7df0bf92a043_iv -in $TRAVIS_BUILD_DIR/.travis/ssh_key.enc -out $TRAVIS_BUILD_DIR/.travis/ssh_key -d
 
-scp -r -i $TRAVIS_BUILD_DIR/.travis/ssh_key -o StrictHostKeyChecking=no $TRAVIS_BUILD_DIR/dist travis@$DEPLOY_HOST_IP:$DEPLOY_DIR_DEV/staging
-ssh -i $TRAVIS_BUILD_DIR/.travis/ssh_key -o StrictHostKeyChecking=no travis@$DEPLOY_HOST_IP $SSH_COMMAND
+echo scp -r -i $TRAVIS_BUILD_DIR/.travis/ssh_key -o StrictHostKeyChecking=no $TRAVIS_BUILD_DIR/dist travis@$DEPLOY_HOST_IP:$DEPLOY_DIR_DEV/staging
+#ssh -i $TRAVIS_BUILD_DIR/.travis/ssh_key -o StrictHostKeyChecking=no travis@$DEPLOY_HOST_IP $SSH_COMMAND
