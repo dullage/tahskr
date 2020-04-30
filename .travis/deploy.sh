b59ad3214176fd0e9938b0c1897b0509af2fd0d2
Adam Dullage <adam@dullage.com>
2020-04-30 21:15:16 +0100

    rsync Deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,11 +2,20 @@
 
 set -ev
 
+# if [[ $TRAVIS_BRANCH == "develop" ]]
+# then
+#     deploy_env=dev
+# elif [[ $TRAVIS_TAG != "" ]]
+# then
+#     deploy_env=prod
+# else
+#     exit 1
+# fi
+
+deploy_env=dev
+
 choco install openssl.light -y
 
 openssl aes-256-cbc -K $encrypted_7df0bf92a043_key -iv $encrypted_7df0bf92a043_iv -in $TRAVIS_BUILD_DIR/.travis/ssh_key.enc -out $TRAVIS_BUILD_DIR/.travis/ssh_key -d
 
-scp -r -i $TRAVIS_BUILD_DIR/.travis/ssh_key -o StrictHostKeyChecking=no $TRAVIS_BUILD_DIR/dist travis@$DEPLOY_HOST_IP:$DEPLOY_TARGET_DIR/staging
-
-ssh_command="rm -R $DEPLOY_TARGET_DIR/dist && mv $DEPLOY_TARGET_DIR/staging/dist $DEPLOY_TARGET_DIR"
-ssh -i $TRAVIS_BUILD_DIR/.travis/ssh_key -o StrictHostKeyChecking=no travis@$DEPLOY_HOST_IP $ssh_command
+rsync --delete -e "ssh -i $TRAVIS_BUILD_DIR/.travis/ssh_key -o StrictHostKeyChecking=no" $TRAVIS_BUILD_DIR/dist travis@$DEPLOY_HOST_IP:$DEPLOY_TARGET_DIR/$deploy_env
