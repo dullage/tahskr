34b6623d480e80d0c099d1c487be9494e1e0d7a5
Adam Dullage <adam@dullage.com>
2020-04-30 21:26:35 +0100

    Add -r switch to rsync

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -18,4 +18,5 @@ choco install openssl.light -y
 
 openssl aes-256-cbc -K $encrypted_7df0bf92a043_key -iv $encrypted_7df0bf92a043_iv -in $TRAVIS_BUILD_DIR/.travis/ssh_key.enc -out $TRAVIS_BUILD_DIR/.travis/ssh_key -d
 
-rsync --delete -e "ssh -i $TRAVIS_BUILD_DIR/.travis/ssh_key -o StrictHostKeyChecking=no" $TRAVIS_BUILD_DIR/dist travis@$DEPLOY_HOST_IP:$DEPLOY_TARGET_DIR/$deploy_env
+# rsync -r --delete -e "ssh -i $TRAVIS_BUILD_DIR/.travis/ssh_key -o StrictHostKeyChecking=no" $TRAVIS_BUILD_DIR/dist travis@$DEPLOY_HOST_IP:$DEPLOY_TARGET_DIR/$deploy_env
+rsync -r --delete -e "ssh -i $TRAVIS_BUILD_DIR/.travis/ssh_key -o StrictHostKeyChecking=no" $TRAVIS_BUILD_DIR travis@$DEPLOY_HOST_IP:$DEPLOY_TARGET_DIR/$deploy_env
