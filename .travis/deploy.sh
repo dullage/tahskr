0a06c587921f6c62bff31ba1f9de240737ae08f1
Adam Dullage <adam@dullage.com>
2020-04-30 07:53:39 +0100

    Github Releases Test

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,7 +6,7 @@ choco install openssl.light -y
 
 openssl aes-256-cbc -K $encrypted_7df0bf92a043_key -iv $encrypted_7df0bf92a043_iv -in $TRAVIS_BUILD_DIR/.travis/ssh_key.enc -out $TRAVIS_BUILD_DIR/.travis/ssh_key -d
 
-scp -r -i $TRAVIS_BUILD_DIR/.travis/ssh_key -o StrictHostKeyChecking=no $TRAVIS_BUILD_DIR/dist $TRAVIS_BUILD_DIR/builds/tahskr.zip travis@$DEPLOY_HOST_IP:$DEPLOY_TARGET_DIR/staging
+scp -r -i $TRAVIS_BUILD_DIR/.travis/ssh_key -o StrictHostKeyChecking=no $TRAVIS_BUILD_DIR/dist travis@$DEPLOY_HOST_IP:$DEPLOY_TARGET_DIR/staging
 
 ssh_command="rm -R $DEPLOY_TARGET_DIR/dist && mv $DEPLOY_TARGET_DIR/staging/dist $DEPLOY_TARGET_DIR"
 ssh -i $TRAVIS_BUILD_DIR/.travis/ssh_key -o StrictHostKeyChecking=no travis@$DEPLOY_HOST_IP $ssh_command
