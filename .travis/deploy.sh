cc690a3d7a512effe20650750fbef6cf0e660508
Adam Dullage <adam@dullage.com>
2020-05-04 08:02:32 +0100

    Set permissions on rsync

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -16,4 +16,4 @@ choco install openssl.light -y
 
 openssl aes-256-cbc -K $encrypted_7df0bf92a043_key -iv $encrypted_7df0bf92a043_iv -in $TRAVIS_BUILD_DIR/.travis/ssh_key.enc -out $TRAVIS_BUILD_DIR/.travis/ssh_key -d
 
-rsync -r --delete -e "ssh -i $TRAVIS_BUILD_DIR/.travis/ssh_key -o StrictHostKeyChecking=no" ./dist/ travis@$DEPLOY_HOST_IP:$DEPLOY_TARGET_DIR/$deploy_env
+rsync -r --delete --perms --chmod=775 -e "ssh -i $TRAVIS_BUILD_DIR/.travis/ssh_key -o StrictHostKeyChecking=no" ./dist/ travis@$DEPLOY_HOST_IP:$DEPLOY_TARGET_DIR/$deploy_env
