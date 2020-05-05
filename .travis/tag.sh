6d44791da7d6e42f7301f171f2e7f4a635fe439e
Adam Dullage <adam@dullage.com>
2020-05-05 17:47:46 +0100

    Existing Tag Check Change

--- .travis/tag.sh
+++ .travis/tag.sh
@@ -2,24 +2,27 @@
 
 set -ev
 
+# Deployments (and therefore this script) also run on the develop branch but we only want to tag on master
 if [[ $TRAVIS_BRANCH == "master" ]]
 then
+    # Extract the version from the package.json file
     version=$(cat $TRAVIS_BUILD_DIR/package.json | jq -r ".version")
     echo Extracted Version Number: $version
 
+    # Fetch all tags from the remote
     git fetch --tags
 
-    git rev-parse "$version"
+    # See if any tags already match the version
+    existing_tag_count=$(git tag | grep $version | wc -l)
 
-    # Check if tag already exists with version number 
-    if git rev-parse "$version" >/dev/null 2>&1
+    if [[ $existing_tag_count == 0 ]]
     then
-        echo "Tag already exists for this version!"
-        exit 1
-    else
         git config user.email "travis@travis-ci.com"
         git config user.name "Travis CI"
 
         git tag -a "$version" -m "$version"
+    else
+        echo "Tag already exists for this version!"
+        exit 1
     fi
 fi
