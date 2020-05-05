601ac84147e418bbcae72501b7c9c93e6cd464d3
Adam Dullage <adam@dullage.com>
2020-05-05 18:22:57 +0100

    Tag as deploy to prevent duplication

--- .travis/tag.sh
+++ .travis/tag.sh
@@ -1,30 +1,16 @@
 #!/bin/bash
 
+# Note: This script cannot run as a before_deploy script otherwise it would run for every deployment
+
 set -ev
 
-# Deployments (and therefore this script) also run on the develop branch but we only want to tag on master
-if [[ $TRAVIS_BRANCH == "master" ]]
-then
-    # Extract the version from the package.json file
-    version=$(cat $TRAVIS_BUILD_DIR/package.json | jq -r ".version")
-    echo Extracted Version Number: $version
+git config user.email "travis@travis-ci.com"
+git config user.name "Travis CI"
 
-    # Fetch all tags from the remote
-    git fetch --tags
+version=$(cat $TRAVIS_BUILD_DIR/package.json | jq -r ".version")
+echo Extracted Version Number: $version
 
-    # See if any tags already match the version
-    git tag
-    existing_tag_count=$(git tag | grep $version | wc -l)
-    echo $existing_tag_count
+git tag
+git fetch --tags
 
-    if [[ $existing_tag_count == 0 ]]
-    then
-        git config user.email "travis@travis-ci.com"
-        git config user.name "Travis CI"
-
-        git tag -a "$version" -m "$version"
-    else
-        echo "Tag already exists for this version!"
-        exit 1
-    fi
-fi
+git tag -a "$version" -m "$version"
