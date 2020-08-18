86ea6c1ec031c15070f402a0eb6ad6ace8df2cd1
Adam Dullage <adam@dullage.com>
2020-08-18 20:58:35 +0100

    Update Dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -2,45 +2,52 @@ const path = require("path");
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const VueLoaderPlugin = require("vue-loader/lib/plugin");
-const CopyPlugin = require('copy-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "development",
   devtool: "inline-source-map",
   devServer: {
-    contentBase: "./dist"
+    contentBase: "./dist",
   },
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "index.bundle.js"
+    filename: "index.bundle.js",
   },
   resolve: {
     alias: {
-      icons: path.resolve(__dirname, "node_modules/vue-material-design-icons")
-    }
+      icons: path.resolve(__dirname, "node_modules/vue-material-design-icons"),
+    },
   },
   module: {
     rules: [
       {
         test: /\.vue$/,
-        use: "vue-loader"
+        use: "vue-loader",
       },
       {
         test: /\.css$/,
-        use: ["vue-style-loader", "css-loader"]
+        use: ["vue-style-loader", "css-loader"],
       },
       {
         test: /\.scss$/,
-        use: ["vue-style-loader", "css-loader", "postcss-loader", "sass-loader"]
-      }
-    ]
+        use: [
+          "vue-style-loader",
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html"
+      template: "./src/index.html",
     }),
     new VueLoaderPlugin(),
-    new CopyPlugin([{ from: path.resolve(__dirname, "src/icons") }])
-  ]
+    new CopyPlugin({
+      patterns: [{ from: path.resolve(__dirname, "src/icons") }],
+    }),
+  ],
 };
